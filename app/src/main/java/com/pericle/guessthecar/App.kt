b52ea30cbbd86a8d305909de4211380d87105150
Dmitry <kyloren228@gmail.com>
2019-08-19 15:20:18 +0300

    Make app prefetch images at start

--- app/src/main/java/com/pericle/guessthecar/App.kt
+++ app/src/main/java/com/pericle/guessthecar/App.kt
@@ -1,6 +1,9 @@
 package com.pericle.guessthecar
 
 import android.app.Application
+import coil.Coil
+import coil.api.load
+import coil.request.CachePolicy
 import com.google.firebase.firestore.FirebaseFirestore
 import com.pericle.guessthecar.database.Car
 import timber.log.Timber
@@ -26,10 +29,21 @@ class App : Application() {
                 if (it != null) {
                     cars = it.toObjects(Car::class.java)
                 }
+                prefetch()
             }
             .addOnFailureListener {
                 Timber.i(it.message, "Fetching cars failed: %s")
             }
+    }
+
+    private fun prefetch() {
+        for (car in cars) {
+            for (img in car.images){
+                Coil.load(this, img) {
+                    memoryCachePolicy(CachePolicy.DISABLED)
+                }
+            }
+        }
     }
 
 }
